1c3122efc2e5801b3616b0729694e6d8cf9fed14
Isuru Fernando <isuruf@gmail.com>
2025-01-15 16:07:58 +0530

    Fix windows activate scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ FOR %%F IN (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
     if errorlevel 1 exit 1
     sed "s/@PY_LIMITED_API/%PY_LIMITED_API%/g" %RECIPE_DIR%\scripts\%%F.bat >> %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if errorlevel 1 exit 1
     :: We also copy .sh scripts to be able to use them
     :: with POSIX CLI on Windows.
     sed "s/@PY_LIMITED_API/%PY_LIMITED_API%/g" %RECIPE_DIR%\scripts\%%F.sh >> %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
